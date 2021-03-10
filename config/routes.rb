f1cc4a4ca6777d3dd91aebb2e66100f04f3b2d15
NAME <MAIL ADDRESS>
2021-03-10 12:25:30 +0000

    pictgram

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  get 'topic/new'
+  get 'sessions/new'
+  
+  root 'pages#index'
+  get 'pages/help'
+  
+  resources :users
+  resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  resources :users
+  resources :topics
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
